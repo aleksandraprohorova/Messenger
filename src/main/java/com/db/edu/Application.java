a6ab8068edcd415266892b0d547687749c2a908c
verdoga55 <spaceivanoff@yandex.ru>
2021-11-01 13:40:39 +0300

    Application

--- src/main/java/com/db/edu/Application.java
+++ src/main/java/com/db/edu/Application.java
@@ -1,13 +1,18 @@
 package com.db.edu;
 
+import ch.qos.logback.core.net.SocketConnector;
+
 import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.Socket;
 
 public class Application {
+    private static final String hostConnection = "127.0.0.1";
+    private static final Integer port = 250;
+
     public static void main(String[] argc) {
         try{
-            final Socket socket = new Socket("127.0.0.1", 250);
+            final Socket socket = new Socket(hostConnection, port);
             final DataInputStream input = new DataInputStream(
                     new BufferedInputStream(socket.getInputStream()));
             final DataOutputStream out = new DataOutputStream(
