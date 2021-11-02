ff0faf44401eaeb5133e3f77d12ec5b6fe1e3bc6
Che1012 <t-minibaev@mail.ru>
2021-11-02 13:52:30 +0300

    Add timer to client

--- src/main/java/com/db/edu/Client.java
+++ src/main/java/com/db/edu/Client.java
@@ -3,9 +3,12 @@ package com.db.edu;
 import com.db.edu.connection.Proxy;
 import com.db.edu.connection.Connector;
 import com.db.edu.creater.MessageCreater;
+import com.db.edu.message.HistMessage;
 import com.db.edu.message.Message;
 
 import java.io.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Client {
     public static void main(String[] argc) {
@@ -15,15 +18,48 @@ public class Client {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Listener listener = new Listener(proxy);
         listener.start();
+        String outputHistory = "";
+        boolean checkHistory = false;
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Message message = creator.createMessage("id", "data", "/hist");
+                try {
+                    proxy.send(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, 5 * 1000);
         while (true) {
             try {
                 if (listener.hasMessage()) {
                     Message answer = listener.getMessage();
-                    System.out.println(answer.getReturnMessage());
+                    if (answer instanceof HistMessage) {
+                        String answerString = answer.getReturnMessage();
+                        int answerLength = answerString.length();
+
+                        if (checkHistory) {
+                            System.out.println(answerString);
+                            checkHistory = false;
+                        } else {
+                            if (!outputHistory.equals(answer.getReturnMessage())) {
+                                System.out.println(answerString.substring(outputHistory.length(), answerLength - 1));
+                                outputHistory = answer.getReturnMessage();
+                            }
+                        }
+                    }
                     listener.resetMessage();
                 }
                 if (reader.ready()) {
                     Message message = creator.createMessage("id", "data", reader.readLine());
+                    if (message instanceof HistMessage) {
+                        checkHistory = true;
+                    }
                     proxy.send(message);
                 }
 
