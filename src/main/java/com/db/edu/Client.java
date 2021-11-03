f59bec2916106695a13f78dd5615b2dedb3b8aab
George <gshervashidze1999@yandex.ru>
2021-11-03 09:42:22 +0300

    fix anything

--- src/main/java/com/db/edu/Client.java
+++ src/main/java/com/db/edu/Client.java
@@ -9,11 +9,9 @@ import com.db.edu.message.Message;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
+
+import static java.lang.System.lineSeparator;
 
 public class Client {
     public static void main(String[] argc) {
@@ -124,10 +122,38 @@ public class Client {
         }
 
         private static void createInputConsole() {
-            try {
-                Runtime.getRuntime().exec("cmd /c start runjava.bat");
-            } catch (IOException e) {
-                e.printStackTrace();
+            System.out.println("test");
+            Scanner scanner = new Scanner(System.in);
+
+            final File folder = new File(".");
+            String messageBufferName = getMessageBufferName(folder);
+
+            while (true) {
+                String line = scanner.nextLine();
+                try(FileWriter writer = new FileWriter(messageBufferName, true))
+                {
+                    writer.write(line + lineSeparator());
+                    writer.flush();
+                }
+                catch(IOException ex){
+                    System.out.println(ex.getMessage());
+                }
             }
         }
+
+    private static String getMessageBufferName(final File folder) {
+        List<String> files = new ArrayList<>();
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isFile()) {
+                String fileName = fileEntry.toString();
+                if (fileName.contains("messageBuffer")) {
+                    files.add(fileName);
+                }
+            }
+        }
+
+        Collections.sort(files);
+        System.out.println(files.get(files.size() - 1));
+        return files.get(files.size() - 1);
+    }
 }
